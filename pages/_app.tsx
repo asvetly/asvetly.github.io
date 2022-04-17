70fcd3b9e3cda9f654a4b99dcc31401b196f56f6
Светличный Александр Викторович <asvetly1337@gmail.com>
2022-04-17 12:21:13 +0300

    Add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import type {AppProps} from 'next/app';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 
 import '../styles/globals.css';
 
 function MyApp({Component, pageProps}: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = url => {
+            window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+                page_path: url,
+            })
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return <Component {...pageProps} />;
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,6 +47,23 @@ export default class MyDocument extends Document {
                         href="https://fonts.gstatic.com"
                         crossOrigin="true"
                     />
+
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                  page_path: window.location.pathname,
+                                });
+                              `,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
